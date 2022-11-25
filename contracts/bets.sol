40352a41cd5b21a7f37d38fae3470117c5b243bf
RjvPivs <Savchick-Denchick@mail.ru>
2022-11-25 22:27:15 +0300

    Remastered

---
+++ contracts/bets.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract bets is ERC20 {
+    constructor(string memory tokenName, string memory tokenSymbol)
+        ERC20(tokenName, tokenSymbol){}
+
+    
+}
